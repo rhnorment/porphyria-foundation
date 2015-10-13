7b57d6da03279830ebd180bd9753e1d950eb2e35
Hunt Norment <hunt@sherpa.red>
2015-10-13 08:27:56 -0500

    config fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ack::Timeout.timeout = (ENV['RACK_TIMEOUT'] || 10).to_i
+  Rack::Timeout.timeout = (ENV['RACK_TIMEOUT'] || 10).to_i
 end
