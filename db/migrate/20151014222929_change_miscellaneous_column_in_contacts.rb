58e776544488bbe47bd1a240771d6ce7ad6979d7
Hunt Norment <hunt@sherpa.red>
2015-10-14 17:36:39 -0500

    config database for data import

--- db/migrate/20151014222929_change_miscellaneous_column_in_contacts.rb
+++
@@ -1,9 +0,0 @@
-class ChangeMiscellaneousColumnInContacts < ActiveRecord::Migration
-  def self.up
-    change_column   :contacts,  :miscellaneous,  :text,  default: ''
-  end
-
-  def self.down
-    change_column   :contacts,  :miscellaneous,  :string,  default: ''
-  end
-end
