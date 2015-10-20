572abb6642d28be647a565e4fad0bceb89686ecc
Hunt Norment <hunt@sherpa.red>
2015-10-20 17:31:29 -0500

    remove DOB and research columns.  change misc to text column

--- db/migrate/20151020154922_remove_research_from_contacts.rb
+++ db/migrate/20151020154922_remove_research_from_contacts.rb
@@ -1,13 +1,13 @@
 class RemoveResearchFromContacts < ActiveRecord::Migration
   def self.up
     remove_column   :contacts,  :research
-    change_column   :contacts,  :date_of_birth, :date
+    remove_column   :contacts,  :date_of_birth
     change_column   :contacts,  :miscellaneous, :text
   end
 
   def self.down
     add_column      :contacts, :research, :string, default: ''
-    change_column   :contacts,  :date_of_birth, :string
+    add_column      :contacts,  :date_of_birth, :string
     change_column   :contacts,  :miscellaneous, :string
   end
 end
