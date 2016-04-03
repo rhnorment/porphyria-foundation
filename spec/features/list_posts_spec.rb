b12a7bbe64f1a962ae230d07a5c086f3b68022c1
Hunt Norment <hunt@sherpa.red>
2016-04-03 16:33:07 -0500

    capbybara specs pass on post index page

--- spec/features/list_posts_spec.rb
+++ spec/features/list_posts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'list postse', type: :feature do
+describe 'list posts', type: :feature do
 
   let!(:post) { create(:published_post) }
   let!(:post_without_image) { create(:post_without_image) }
@@ -9,11 +9,43 @@ describe 'list postse', type: :feature do
 
   context 'the post contains a primary image' do
     it 'contains the correct attributes and link' do
+      within("div##{post.id}") do
+        expect(page).to have_selector('div.media')
 
-      print page.html
+        expect(page).to have_text(post.title)
+        expect(page).to have_text(post.author)
+        expect(page).to have_text(post.published_at.to_date)
+        expect(page).to have_text(post.body)
+
+        expect(page).to have_link('Read more')
+      end
     end
   end
 
-  context 'the post does not contain a primary image'
+  context 'the post does not contain a primary image' do
+    it 'contains the correct attributes and link' do
+      within("div##{post_without_image.id}") do
+        expect(page).to have_selector('div.media')
+
+        expect(page).to have_text(post_without_image.title)
+        expect(page).to have_text(post_without_image.author)
+        expect(page).to have_text(post_without_image.published_at.to_date)
+        expect(page).to have_text(post_without_image.body)
+
+        expect(page).to have_link('Read more')
+      end
+    end
+  end
+
+  it 'should render the sidebar' do
+    expect(page).to have_link('News')
+    expect(page).to have_link('Blog')
+    expect(page).to have_link('Member Stories')
+    expect(page).to have_link('Get Involved')
+
+    expect(page).to have_field('search')
+
+    expect(page).to have_text('Tags')
+  end
 
 end
