de4a4f8e4d67a71d233ced52977e3018eb896c84
Hunt Norment <hunt@sherpa.red>
2017-02-24 15:12:04 -0500

    fix migration file

--- db/migrate/20151020224026_add_dob_to_contacts.rb
+++ db/migrate/20151020224026_add_dob_to_contacts.rb
@@ -1,8 +1,6 @@
 class AddDobToContacts < ActiveRecord::Migration
   def self.up
     add_column  :contacts,  :date_of_birth,   :date
-
-    Rake::Task['import_data:create_date_of_birth'].invoke
   end
 
   def self.down
